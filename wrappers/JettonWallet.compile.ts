138af1671126b36bf9060288282dc621b22ccab1
dykim <dykim@burritowallet.com>
2024-12-04 14:29:34 +0900

    updated README.md

--- wrappers/JettonWallet.compile.ts
+++ wrappers/JettonWallet.compile.ts
@@ -1,6 +1,5 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/burger_blast_token.fc'],
+    targets: ['contracts/jetton-wallet.fc'],
 };
